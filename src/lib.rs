1e20fa5d84a77932da8428ceb4916608625e2d95
an_owl <32559552+an-owl@users.noreply.github.com>
2024-10-30 15:32:23 +1300

    Added prototype implementation

--- src/lib.rs
+++ src/lib.rs
@@ -1,21 +1,78 @@
-#![cfg_attr(not(test),no_std)]
+#![cfg_attr(not(test), no_std)]
 #![feature(allocator_api)]
 #![feature(generic_const_exprs)]
+#![allow(incomplete_features)]
 
 mod slab_meta;
 
 use core::ptr::NonNull;
-use slab_meta::{
-    SlabMetadata,
-    meta_bitmap_size
-};
+use slab_meta::SlabMetadata;
 
-pub struct SlabLike<A: core::alloc::Allocator, T, const SLAB_SIZE: usize> where [(); meta_bitmap_size::<T, SLAB_SIZE>()]: {
+pub use slab_meta::meta_bitmap_size;
+
+pub struct SlabLike<A: core::alloc::Allocator, T, const SLAB_SIZE: usize>
+where
+    [(); meta_bitmap_size::<T>(SLAB_SIZE)]:,
+    [(); slab_count_obj_elements::<T, SLAB_SIZE>()]:,
+{
     alloc: A,
     head: Option<NonNull<Slab<T, SLAB_SIZE>>>,
 }
 
+/// Returns the number of object elements that can be stored in a [Slab]
+///
+/// This fn will panic if the number of object elements is 0.
+pub const fn slab_count_obj_elements<T, const SLAB_SIZE: usize>() -> usize {
+    let elem_size = size_of::<[T; 2]>() / 2;
+    let raw_obj_elem_count = SLAB_SIZE / elem_size;
+
+    let meta_size = slab_meta::size_of_meta::<T>(SLAB_SIZE);
+    let meta_obj_elem_size = meta_size / elem_size;
+
+    let num_obj_elements = raw_obj_elem_count - meta_obj_elem_size;
+    assert!(
+        num_obj_elements > 0,
+        "Slab must have more than one Object elements"
+    ); // This'd be a shit allocator if we had 100% overhead
+
+    num_obj_elements
+}
+
 #[repr(C)]
-struct Slab<T, const SLAB_SIZE: usize> where [(); meta_bitmap_size::<T, SLAB_SIZE>()]: {
+struct Slab<T, const SLAB_SIZE: usize>
+where
+    [(); meta_bitmap_size::<T>(SLAB_SIZE)]:,
+    [(); slab_count_obj_elements::<T, SLAB_SIZE>()]:,
+{
     slab_metadata: SlabMetadata<T, SLAB_SIZE>,
+    obj_elements: [core::mem::MaybeUninit<T>; slab_count_obj_elements::<T, SLAB_SIZE>()],
+}
+
+impl<T, const SLAB_SIZE: usize> Slab<T, SLAB_SIZE>
+where
+    [(); meta_bitmap_size::<T>(SLAB_SIZE)]:,
+    [(); slab_count_obj_elements::<T, SLAB_SIZE>()]:,
+{
+    pub fn new() -> Self
+    where
+        [(); meta_bitmap_size::<T>(SLAB_SIZE)]:,
+        [(); slab_count_obj_elements::<T, SLAB_SIZE>()]:,
+    {
+        Self {
+            slab_metadata: SlabMetadata::new(),
+            obj_elements: [const { core::mem::MaybeUninit::uninit() };
+                slab_count_obj_elements::<T, SLAB_SIZE>()],
+        }
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn check_slab_size() {
+        let slab = Slab::<u128, 64>::new();
+        assert_eq!(slab.obj_elements.len(), 2);
+    }
 }
